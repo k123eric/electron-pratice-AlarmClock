7d986ff3d274284311b16a9ed15d9bb88d42c45c
k123eric <55881329+k123eric@users.noreply.github.com>
2020-12-11 21:22:07 +0800

    feat. 功能完成

---
+++ main.js
@@ -0,0 +1,88 @@
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  Menu,
+} = require('electron');
+
+const path = require('path');
+const url = require('url');
+let appIcon;
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  // darwin = MacOS
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
+/**
+ * @constructor
+ */
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 400,
+    height: 400,
+    maximizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true,
+      }),
+  );
+
+  // Open DevTools.
+  // win.webContents.openDevTools()
+
+  // When Window Close.
+  win.on('closed', () => (win = null));
+
+  // When Window Minimize
+  win.on('minimize', () => {
+    win.hide();
+  });
+
+  // Create Tray
+  createTray();
+}
+
+/**
+ * @constructor
+ */
+function createTray() {
+  const iconPath = path.join(__dirname, 'clock.ico');
+
+  const contextMenu = Menu.buildFromTemplate([{
+    label: 'AlarmClock',
+    click() {
+      win.show();
+    },
+  },
+  {
+    label: 'Quit',
+    click() {
+      win.removeAllListeners('close');
+      win.close();
+    },
+  },
+  ]);
+
+  appIcon = new Tray(iconPath);
+  appIcon.setToolTip('Alarm Clock');
+  appIcon.setContextMenu(contextMenu);
+}
